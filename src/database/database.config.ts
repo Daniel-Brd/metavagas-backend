435d4ca8a2c1f0e461bf4d76304960e59ddbe1d7
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-09-26 20:40:09 -0300

    chore: add @nestsj/config dependecy

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,25 +1,27 @@
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 export default <TypeOrmModuleAsyncOptions>{
-    imports:[ConfigModule],
-    inject:[ConfigService],
+  imports: [ConfigModule],
+  inject: [ConfigService],
 
-    useFactory: async (
-        configService:ConfigService,
-        ): Promise<TypeOrmModuleOptions> => {
-
-            return <PostgresConnectionOptions>{
-                type:'postgres',
-                host: configService.get('DB_HOST'),
-                port: +configService.get('DB_PORT'),
-                username: configService.get('DB_USERNAME'),
-                password: configService.get('DB_PASSWORD'),
-                database: configService.get('DB_DATABASE'),
-                extra: {ssl: true},
-                entities:[],
-                synchronize:true
-            }
-        },
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => {
+    return <PostgresConnectionOptions>{
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: +configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_DATABASE'),
+      extra: { ssl: true },
+      entities: [],
+      synchronize: true,
+    };
+  },
 };
