d54003e3edf11e8fc828c1e474dda30abe654583
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-09-26 22:01:25 -0300

    feat: add vacancies manyToOne company relation

--- src/database/entities/vacancies.entity.ts
+++ src/database/entities/vacancies.entity.ts
@@ -1,37 +1,45 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Company } from './company.entity';
 
 @Entity()
 export class Vacancy {
-    @PrimaryGeneratedColumn('uuid')
-    id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
 
-    @Column({type:'varchar', length: 80, nullable: false})
-    vacancyRole: string
+  @Column({ type: 'varchar', length: 80, nullable: false })
+  vacancyRole: string;
 
-    @Column({type:'int', nullable: false})
-    wage: number;
+  @Column({ type: 'int', nullable: false })
+  wage: number;
 
-    @Column({type: 'varchar', length: 80, nullable: false})
-    location: string;
+  @Column({ type: 'varchar', length: 80, nullable: false })
+  location: string;
 
-    @Column({type:'varchar', length: 80, nullable: false})
-    vacancyType: string;
+  @Column({ type: 'varchar', length: 80, nullable: false })
+  vacancyType: string;
 
-    @Column({type:'text', nullable: false})
-    vacancyDescription: string;
+  @Column({ type: 'text', nullable: false })
+  vacancyDescription: string;
 
-    @Column({type:'varchar', length: 64, nullable: false})
-    level: string;
+  @Column({ type: 'varchar', length: 64, nullable: false })
+  level: string;
 
-    @Column({type:'int', nullable: false})
-    companyId: number;
+  @ManyToOne(() => Company)
+  companyId: Company;
 
-    @Column({type:'int', nullable: false})
-    advertiserId: number;
+  @Column({ type: 'int', nullable: false })
+  advertiserId: number;
 
-    @CreateDateColumn()
-    createdAt: Date;
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @UpdateDateColumn()
-    updateAt: Date;
+  @UpdateDateColumn()
+  updateAt: Date;
 }
