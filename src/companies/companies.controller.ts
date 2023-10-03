9e3cf1664c6057f34dba82fc0ea3ebb7a74bab6e
Osouzaa <gabrielsouzaalves750@gmail.com>
2023-10-03 18:58:54 -0300

    docs:  Initial creation of Api documentation

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -2,33 +2,43 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('companies')
 @Controller('companies')
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new company'})
   create(@Body() createCompanyDto: CreateCompanyDto) {
     return this.companiesService.create(createCompanyDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'List all registered companies' })
   findAll() {
     return this.companiesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Search for a company by ID' })
   findOne(@Param('id') id: string) {
     return this.companiesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a company by ID' })
   update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
     return this.companiesService.update(+id, updateCompanyDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Deletes a company by ID' })
   remove(@Param('id') id: string) {
     return this.companiesService.remove(+id);
   }
 }
+
+
+
--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,26 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCompanyDto {
+  @ApiProperty({
+    type: String,
+    example: 'TechSoluções LTDA',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    type: String,
+    example: 'Contagem',
+  })
   @IsNotEmpty()
   @IsString()
   city: string;
 
+  @ApiProperty({
+    type: String,
+    example: 'Minas Gerais',
+  })
   @IsNotEmpty()
   @IsString()
   state: string;
 
+  @ApiProperty({
+    type: String,
+    example: 'Rua B',
+  })
   @IsNotEmpty()
   @IsString()
   address: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDateString()
   foundedAt: Date;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   description: string;
