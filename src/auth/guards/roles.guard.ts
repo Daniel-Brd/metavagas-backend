712ac9d30a6c92392ae1c210d8bdd0e288a048ec
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-10-03 18:01:27 -0300

    feat: create roles guard

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, HttpException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { RoleEnum } from '../../enums/role.enum';
 import { ROLES_KEY } from '../../utils/constants';
@@ -8,15 +8,33 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) { }
 
   canActivate(context: ExecutionContext): boolean {
+
+    const { user, params } = context.switchToHttp().getRequest()
+
     const requiredRoles = this.reflector.getAllAndOverride<RoleEnum[]>(
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
     );
+
+    let isSelfPermission = this.reflector.getAllAndOverride(
+      'selfPermission',
+      [context.getHandler(), context.getClass()]
+    )
+
+    isSelfPermission ? isSelfPermission = Object.values(isSelfPermission)[0] : false
+
     if (!requiredRoles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest();
 
-    return requiredRoles.some(role => user.userRole?.includes(role));
+    if (isSelfPermission && params.id === user.userId) {
+      return true
+    }
+
+    if (requiredRoles.some(role => user.userRole?.includes(role))) {
+      return true
+    }
+
+    throw new HttpException('You do not have permission to this action.', 401);
   }
 }
