ca5517ec4da7a2bef2a626aed2ce3bce68e365b1
Osouzaa <gabrielsouzaalves750@gmail.com>
2023-10-03 19:02:05 -0300

    docs: Initial creation Api swagger documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,39 +1,69 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Controller, Get,Post,Body,Patch,Param,Delete,ParseUUIDPipe} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {ApiBearerAuth,ApiOperation,ApiResponse,ApiTags,} from '@nestjs/swagger';
 
+
+@ApiTags('users')
+@ApiBearerAuth('users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'User created successfully', type: CreateUserDto })
+  @ApiResponse({ status: 400, description: 'Invalid parameters',  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'List all registered users' })
+  @ApiResponse({ status: 200, description: 'Lists of all users' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id/profile')
+  @ApiOperation({ summary: 'Search user profile by ID' })
+  @ApiResponse({ status: 200, description: 'User profile found', type: CreateUserDto })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'User not found' })
   findProfile(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findProfile(id);
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'User found' })
+  @ApiResponse({ status: 404, description: 'User not found', })
+  @ApiOperation({ summary: 'Search a user by ID' })
   findById(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findById(id);
   }
 
   @Patch(':id')
-  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
+  @ApiOperation({ summary: 'Update a user by ID' })
+  @ApiResponse({ status: 200, description: 'User updated' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Patch(':id/delete')
+  @ApiOperation({ summary: 'Deactivate a user by ID' })
+  @ApiResponse({ status: 200, description: 'User successfully deactivated' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   softDelete(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.softDelete(id);
   }
+  
 }
--- src/vacancies/vacancies.controller.ts
+++ src/vacancies/vacancies.controller.ts
@@ -2,33 +2,41 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { VacanciesService } from './vacancies.service';
 import { CreateVacancyDto } from './dto/create-vacancy.dto';
 import { UpdateVacancyDto } from './dto/update-vacancy.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('vacancies')
 @Controller('vacancies')
 export class VacanciesController {
   constructor(private readonly vacanciesService: VacanciesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new vacancy as an advertiser'})
   create(@Body() createVacancyDto: CreateVacancyDto) {
     return this.vacanciesService.create(createVacancyDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Search all registered vacancies'})
   findAll() {
     return this.vacanciesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Search for a vacancy by an ID'})
   findOne(@Param('id') id: string) {
     return this.vacanciesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a vacancy by an ID'})
   update(@Param('id') id: string, @Body() updateVacancyDto: UpdateVacancyDto) {
     return this.vacanciesService.update(+id, updateVacancyDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Deletes a vacancy by an ID'})
   remove(@Param('id') id: string) {
     return this.vacanciesService.remove(+id);
   }
 }
+
