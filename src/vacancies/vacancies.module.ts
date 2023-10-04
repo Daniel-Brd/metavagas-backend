0aa94a63744066d2dbe88335260fa03151d35c85
Neto <picilianovasconcelos@gmail.com>
2023-10-03 21:49:09 -0300

    fix: add dependencies

--- src/vacancies/vacancies.module.ts
+++ src/vacancies/vacancies.module.ts
@@ -3,9 +3,13 @@ import { VacanciesService } from './vacancies.service';
 import { VacanciesController } from './vacancies.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Vacancy } from '../database/entities/vacancies.entity';
+import { Company } from '../database/entities/company.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Vacancy])],
+  imports: [TypeOrmModule.forFeature([Vacancy, Company]),
+  UsersModule
+],
   controllers: [VacanciesController],
   providers: [VacanciesService],
 })
