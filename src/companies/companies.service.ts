35b7872c75968c51043409227505710d0c70e280
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-10-04 23:35:03 -0300

    fix: fix find all companies query

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,13 +4,14 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Company } from '../database/entities/company.entity';
 import { Like, Repository } from 'typeorm';
+import { QueryCompanyDTO } from './dto/query-company.dto';
 
 @Injectable()
 export class CompaniesService {
   constructor(
     @InjectRepository(Company)
     private companyRepository: Repository<Company>
-  ){}
+  ) { }
 
   async create(createCompanyDto: CreateCompanyDto) {
     try {
@@ -20,41 +21,49 @@ export class CompaniesService {
       return newCompany;
 
     } catch (error: any) {
-      
+
       throw new HttpException(error.message || 'Internal server error.', error.status || 500)
     }
   }
 
-  async findAll(name?: string): Promise<Company[]> {
-    if (name) {
-        return await this.companyRepository.find({
-            where: { name: Like(`%${name}%`) },
-            relations: ['vacancies']
-        });
-    }
-    return await this.companyRepository.find({ relations: ['vacancies'] });
-}
+  async findAll(query?: QueryCompanyDTO): Promise<Company[]> {
 
-  async findById(id: string): Promise<Company>{
+
+    if (!Object.keys(query).length) {
+      return this.companyRepository.find({ relations: ['vacancies'] });
+    }
+
+    if (query.name) {
+      return this.companyRepository.find({
+        where: {
+          ...query,
+          name: Like(`${query.name}%`)
+        },
+        relations: ['vacancies']
+      });
+    }
+  }
+
+  async findById(id: string): Promise<Company> {
 
     try {
       const company = await this.companyRepository.findOne({
         where: { id: id },
         relations: ['vacancies']
-    });
-  
+      });
+
       if (!company) {
         throw new HttpException('Company not found.', 404)
       }
-  
+
       return company;
     } catch (error: any) {
 
       throw new HttpException(error.message || 'Internal server error', error.status || 500)
     }
-}
+  }
 
-   async update(id: string, updateCompanyDto: UpdateCompanyDto) {
+  async update(id: string, updateCompanyDto: UpdateCompanyDto) {
     const existingCompany = await this.findById(id);
 
     if (!existingCompany) {
@@ -65,4 +74,5 @@ export class CompaniesService {
     const savedCompany = await this.companyRepository.save(updatedCompany);
 
     return savedCompany;
-}}
+  }
+}
