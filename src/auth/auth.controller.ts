d8cffd96e4c6890c31b71ff3f1a2b592635218c2
Osouzaa <gabrielsouzaalves750@gmail.com>
2023-10-08 07:56:28 -0300

    docs: Adding responses to auth endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,26 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'User created successfully', type: CreateUserDto })
+  @ApiResponse({ status: 401, description: 'Invalid email or password.' })
+  @ApiResponse({ status: 400, description: 'Invalid parameters' })
   async register(@Body() payload: CreateUserDto) {
     return await this.authService.register(payload);
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Log in with the username' })
+  @ApiResponse({ status: 201, description: 'User logged in successfully', type: LoginDto })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
   async login(@Body() payload: LoginDto) {
     return await this.authService.login(payload);
   }
