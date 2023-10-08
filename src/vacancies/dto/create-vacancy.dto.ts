11c783a1faa113eb4b3351421e4dee99f75c6467
Osouzaa <gabrielsouzaalves750@gmail.com>
2023-10-08 08:02:49 -0300

    docs: Adding examples to swagger dtos

--- src/vacancies/dto/create-vacancy.dto.ts
+++ src/vacancies/dto/create-vacancy.dto.ts
@@ -2,37 +2,37 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateVacancyDto {
-  @ApiProperty()
+  @ApiProperty({ type: String, example: 'Software Developer' })
   @IsNotEmpty()
   @IsString()
   vacancyRole: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: Number, example: 6000 })
   @IsNotEmpty()
   @IsNumber()
   wage: number;
 
-  @ApiProperty()
+  @ApiProperty({ type: String, example: 'São Paulo, Brazil' })
   @IsNotEmpty()
   @IsString()
   location: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: String, example: 'Full-time' })
   @IsNotEmpty()
   @IsString()
   vacancyType: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: String, example: 'Seeking a talented software developer to join our team for exciting projects.' })
   @IsNotEmpty()
   @IsString()
   vacancyDescription: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: String, example: 'Junior' })
   @IsNotEmpty()
   @IsString()
   level: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: String, example: 'TechSolutions' })
   @IsNotEmpty()
   @IsString()
   companyName: string;
