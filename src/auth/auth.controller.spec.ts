f55b02787c4f77b714d57a281df1273fb9b168f2
Osouzaa <gabrielsouzaalves750@gmail.com>
2023-10-18 16:20:26 -0300

    test: Adds unit tests for login and register

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -5,9 +5,11 @@ import { usersListMock } from '../testing/mocks/users-mocks/users-list.mock';
 import { jwtTokenMock } from '../testing/mocks/jwt-mocks/jwt-token.mock';
 import { authServiceMock } from '../testing/mocks/auth-mocks/auth-service.mock';
 import { createUserMock } from '../testing/mocks/users-mocks/create-user.mock';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,6 +18,7 @@ describe('AuthController', () => {
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
@@ -42,6 +45,13 @@ describe('AuthController', () => {
       const result = await controller.register(createUserMock);
 
       expect(result).toEqual(usersListMock[0]);
+      expect(service.register).toHaveBeenCalledTimes(1);
+      expect(service.register).toHaveBeenCalledWith(createUserMock);
+    });
+
+    it('should throw an exception ', async () => {
+      jest.spyOn(service, 'register').mockRejectedValueOnce(new Error());
+      expect(service.register(createUserMock)).rejects.toThrowError();
     });
   });
 });
