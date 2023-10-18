96a1f56d475321f927d64b360e806ed8dc38cb30
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-10-17 22:35:37 -0300

    feat: create current user type

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,14 +1,26 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '../database/entities/user.entity';
+import { RoleEnum } from '../enums/role.enum';
+import { Vacancy } from '../database/entities/vacancies.entity';
 
-export const getCurrentUserByContext = (ctx: ExecutionContext): User => {
+export const getCurrentUserByContext = (
+  ctx: ExecutionContext,
+): CurrentUserType => {
   if (ctx.getType() === 'http') {
     return ctx.switchToHttp().getRequest().user;
   }
 };
 
+export interface CurrentUserType {
+  userId: string;
+  userName: string;
+  userRole: RoleEnum;
+  isActive: boolean;
+  userEmail: string;
+  vacancies: Vacancy[] | [];
+}
+
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): User => {
+  (data: unknown, ctx: ExecutionContext): CurrentUserType => {
     const user = getCurrentUserByContext(ctx);
 
     return user;
