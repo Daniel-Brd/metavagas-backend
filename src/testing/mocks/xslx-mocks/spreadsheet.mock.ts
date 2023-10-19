6cb48a71c7e38dde459d97d2eb49ef77c3791d82
Daniel Brandão <dev.danielbrandao@gmail.com>
2023-10-19 15:11:05 -0300

    test:  aplly file mock functions to spreadsheet mock

--- src/testing/mocks/xslx-mocks/spreadsheet.mock.ts
+++ src/testing/mocks/xslx-mocks/spreadsheet.mock.ts
@@ -1,14 +1,17 @@
-import { createReadStream } from 'fs';
+import { getFileMock } from '../../../utils/functions';
 
-export const spreadsheetMock: Express.Multer.File = {
+const tempSpreadsheetMock: Partial<Express.Multer.File> = {
   fieldname: 'file',
   originalname: 'mock-vacancy-spreadsheet-xlsx.xlsx',
   encoding: '7bit',
   mimetype: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  buffer: Buffer.from('mock buffer'),
   size: 1111,
-  stream: createReadStream('mock redable stream'),
   destination: 'mock destination',
   filename: 'mock  filename',
   path: 'mock path',
 };
+
+export const spreadsheetMock = getFileMock(
+  tempSpreadsheetMock,
+  'planilha.xlsx',
+);
