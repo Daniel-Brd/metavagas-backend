751f532bfc34eab633b77fc5c18bb09f542680e8
Daniel-Brd <dev.danielbrandao@gmail.com>
2023-10-20 00:21:30 -0300

    feat: create activate user endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -53,6 +53,25 @@ export class UsersController {
     return this.usersService.findById(id);
   }
 
+  @Patch('delete/:id')
+  @Auth([RoleEnum.admin], [PermissionEnum.self])
+  @ApiOperation({ summary: 'Deactivate a user by ID' })
+  @ApiResponse({ status: 200, description: 'User successfully deactivated' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
+  softDelete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.usersService.softDelete(id);
+  }
+
+  @Patch('activate/:id')
+  @Auth([RoleEnum.admin], [PermissionEnum.self])
+  @ApiOperation({ summary: 'activate an user by ID' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
+  activeteUser(@Param('id', ParseUUIDPipe) id: string) {
+    return this.usersService.activateUser(id);
+  }
+
   @Patch(':id')
   @Auth([RoleEnum.admin], [PermissionEnum.self])
   @ApiOperation({ summary: 'Update a user by ID' })
@@ -64,15 +83,5 @@ export class UsersController {
     @Body() updateUserDto: UpdateUserDto,
   ) {
     return this.usersService.update(id, updateUserDto);
-  }
-
-  @Patch(':id/delete')
-  @Auth([RoleEnum.admin], [PermissionEnum.self])
-  @ApiOperation({ summary: 'Deactivate a user by ID' })
-  @ApiResponse({ status: 200, description: 'User successfully deactivated' })
-  @ApiResponse({ status: 404, description: 'User not found' })
-  @ApiResponse({ status: 500, description: 'Internal server error' })
-  softDelete(@Param('id', ParseUUIDPipe) id: string) {
-    return this.usersService.softDelete(id);
   }
 }
