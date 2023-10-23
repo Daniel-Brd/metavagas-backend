4fa8d1450d0e43d19fca292d8867db86e691b078
Osouzaa <gabrielsouzaalves750@gmail.com>
2023-10-23 18:21:26 -0300

    docs:  remove apiProperty from Dtos

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,37 +1,26 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCompanyDto {
-  @ApiProperty({ type: String, example: 'TechSolutions Ltd.' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
-  @ApiProperty({ type: String, example: 'Contagem' })
   @IsNotEmpty()
   @IsString()
   city: string;
 
-  @ApiProperty({ type: String, example: 'Minas Gerais' })
   @IsNotEmpty()
   @IsString()
   state: string;
 
-  @ApiProperty({ type: String, example: 'B Street' })
   @IsNotEmpty()
   @IsString()
   address: string;
 
-  @ApiProperty({ type: Date, example: '2023-07-18' })
   @IsNotEmpty()
   @IsDateString()
   foundedAt: Date;
 
-  @ApiProperty({
-    type: String,
-    example:
-      'TechSolutions Ltd. is a technology company based in Contagem, Minas Gerais, founded on October 6, 2023. We are committed to innovation and excellence.',
-  })
   @IsNotEmpty()
   @IsString()
   description: string;
