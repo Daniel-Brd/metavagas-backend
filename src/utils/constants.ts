5305b820bbe264fd9f2713821cbaa6200aa24d0e
Daniel Brandão <dev.danielbrandao@gmail.com>
2023-10-26 09:10:43 -0300

    fix: wage now have default min and max value

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
 export const ROLES_KEY = 'roles';
 export const PERMISSION_KEY = 'permission';
 export const ACTIVATE_USER_URL = '/users/activate/';
+export const DEFAULT_MIN_WAGE = 1000;
+export const DEFAULT_MAX_WAGE = 30000;
